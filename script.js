0dc405e9414f7cc26063309f28f2fb8578e399c8
Shivam Kumar <shivamkumar271855@gmail.com>
2023-01-18 22:57:52 +0530

    Web page template

---
+++ script.js
@@ -0,0 +1,6 @@
+let openSlide = function (){
+    document.querySelector('.slideMobileContainer').style.display = 'block';
+}
+let closeSlide = function (){
+    document.querySelector('.slideMobileContainer').style.display = 'none';
+}
